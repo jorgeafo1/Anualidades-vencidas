1752780ed4199cf8bde545045d6f47c1f4096ac5
jorgeafo1 <2350467j@umich.mx>
2024-10-24 21:39:22 -0600

    Update AV.R

--- AV.R
+++ AV.R
@@ -24,8 +24,15 @@ t=function(VF, A, r){
 
 # Fórmula para calcular la tasa del periodo, conociendo valor futuro, número de pagos y monto de la anualidad
 
+r= function(VF, A, t) {
+  exit=uniroot(function(r) A * (((1 + r)^t - 1) / r) - VF, lower = 0, upper = 1)$root
+}
 
 
+# Fórmula para calcular el valor actual, conociendo la anualidad, tasa de interés del periodo y el número (plazo) de anualidades.
+VA=function(A, r, t){
+  exit=A * (1 - (1 + r)^(-t)) / r
+}
 
 # Fórmula para calcular la anualidad, conociendo valor actual, tasa del periodo y número de pagos
 
@@ -40,6 +47,14 @@ t=function(VA, A, r){
 }
   
 # Fórmula para calcular la tasa del periodo, conociendo valor actual, número de pagos y monto de la anualidad
+
+r= function(VA, A, t) {
+  exit = uniroot(function(r) A * (((1 + r)^t - 1) / r) - VA, lower = 0, upper = 1)$root
+  return(exit)
+}
+
+
+  
 
 
   
