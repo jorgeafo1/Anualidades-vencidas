a054c5bd276ab3c9c1c85a64a9069544018a7d5b
jorgeafo1 <2350467j@umich.mx>
2024-10-26 11:34:54 -0600

    Update AV.R

--- AV.R
+++ AV.R
@@ -46,6 +46,7 @@ t_VA=function(VA, A, r){
 
 # Fórmula para calcular la tasa del periodo (r), conociendo valor actual
 r_VA=function(VA, A, t) {
-  exit= uniroot(function(r) A * (((1 + r)^t - 1) / r) - VA, lower = 0, upper = 1)$root
+  exit = uniroot(function(r) A * (((1 + r)^t - 1) / r) - VA, lower = 0.0001, upper = 1)$root
   return(exit)
 }
+
