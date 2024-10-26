dccc5187743a5c708b93b3eb532c458bab879f6f
jorgeafo1 <2350467j@umich.mx>
2024-10-26 10:48:29 -0600

    Update AV.R

--- AV.R
+++ AV.R
@@ -2,64 +2,50 @@
 # Autor: Jorge Alberto Ferreyra Ortiz.
 # 24/10/2024
 
-# Fórmula para calcular el valor fúturo 
-
+# Fórmula para calcular el valor futuro (VF)
 VF=function(A, r, t){
   exit=A * ((1 + r)^t - 1) / r
   return(exit)
 }
 
-# Fórmula para calcular la anualidad , conociendo valor futuro, tasa del periodo y número de pagos
-
-A = function(VF, r, t) {
+# Fórmula para calcular la anualidad (A), conociendo valor futuro
+A_VF = function(VF, r, t) {
   exit = (VF * r) / ((1 + r)^t - 1)
   return(exit)
 }
 
-# Fórmula para calcular el Número de pagos o plazo, conociendo valor futuro, anualidad y tasa del periodo
-
-t=function(VF, A, r) {
+# Fórmula para calcular el número de pagos (t), conociendo valor futuro
+t_VF=function(VF, A, r) {
   exit= log((VF * r) / A + 1) / log(1 + r)
   return(exit)
 }
 
-# Fórmula para calcular la tasa del periodo, conociendo valor futuro, número de pagos y monto de la anualidad
-
-r=function(VF, A, t) {
+# Fórmula para calcular la tasa del periodo (r), conociendo valor futuro
+r_VF=function(VF, A, t) {
   exit= uniroot(function(r) A * (((1 + r)^t - 1) / r) - VF, lower = 0, upper = 1)$root
   return(exit)
 }
 
-
-# Fórmula para calcular el valor actual, conociendo la anualidad, tasa de interés del periodo y el número (plazo) de anualidades.
+# Fórmula para calcular el valor actual (VA)
 VA=function(A, r, t){
   exit=A * (1 - (1 + r)^(-t)) / r
   return(exit)
 }
 
-# Fórmula para calcular la anualidad, conociendo valor actual, tasa del periodo y número de pagos
-
-A=function(VA, r, t){
+# Fórmula para calcular la anualidad (A), conociendo valor actual
+A_VA=function(VA, r, t){
   exit=VA * (r / (1 - (1 + r)^(-t)))
   return(exit)
 }
 
-# Fórmula para calcular el número de pagos o plazo, conociendo valor actual, anualidad y tasa del periodo
-
-t=function(VA, A, r){
+# Fórmula para calcular el número de pagos (t), conociendo valor actual
+t_VA=function(VA, A, r){
   exit= log(1 - (VA * r) / A) / log(1 + r)
   return(exit)
 }
-  
-# Fórmula para calcular la tasa del periodo, conociendo valor actual, número de pagos y monto de la anualidad
 
-r=function(VA, A, t) {
+# Fórmula para calcular la tasa del periodo (r), conociendo valor actual
+r_VA=function(VA, A, t) {
   exit= uniroot(function(r) A * (((1 + r)^t - 1) / r) - VA, lower = 0, upper = 1)$root
   return(exit)
 }
-
-
-  
-
-
-  
